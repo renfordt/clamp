a3d65282036db3d4c1d6cb514951a95209ae395d
Jannik Renfordt <jannik.renfordt@ikobb.de>
2024-04-14 20:32:22 +0200

    Add ClampTest unit tests
    This commit introduces a new file, ClampTest.php, which implements unit tests for the clamp function. Various test cases have been created using dataProviderForClampTest to ensure the functionality under different scenarios such as exceeding limits, undercutting limits, and within range values.

---
+++ test/ClampTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace test;
+
+use PHPUnit\Framework\TestCase;
+
+final class ClampTest extends TestCase
+{
+    public static function dataProviderForClampTest(): array
+    {
+        return [
+            [6, 0, 5, 5], // exceed
+            [4, 1, 10, 4], // in range
+            [-2, 3, 7, 3], // undercut
+            [0.25, 0.0, 0.1, 0.1], // exceed
+            [1.45, 1.0, 2.0, 1.45], // in range
+            [-0.14, -0.1, 0.1, -0.1], // undercut
+            ['d', 'a', 'c', 'c'], // exceed
+            ['g', 'a', 'z', 'g'], // in range
+            ['a', 'c', 'f', 'c'], // undercut
+
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderForClampTest[
+     */
+    public function testClamp($value, $min, $max, $expected): void
+    {
+        $this->assertSame($expected, clamp($value, $min, $max));
+    }
+}
