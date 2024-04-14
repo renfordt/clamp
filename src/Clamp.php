de5676b62f3a852166bb817b31dbeb87298d3b18
Jannik Renfordt <jannik.renfordt@ikobb.de>
2024-04-14 21:37:50 +0200

    Remove redundant function checks in Clamp.php
    The unnecessary checks verifying the existence of the 'clamp' and 'clampMinMax' functions have been removed. This is due to the nature of our project's workflow which ensures function duplications do not occur which makes these checks unnecessary.

--- src/Clamp.php
+++ src/Clamp.php
@@ -1,38 +1,34 @@
 <?php
 
-if (!function_exists('clamp')) {
-    /**
-     * Clamps a value between a minimum and maximum range.
-     *
-     * @param  int|float|string  $value  The value to be clamped.
-     * @param  int|float|string  $min  The minimum value of the range.
-     * @param  int|float|string  $max  The maximum value of the range.
-     *
-     * @return int|float|string The clamped value.
-     */
-    function clamp(int|float|string $value, int|float|string $min, int|float|string $max): int|float|string
-    {
-        if ($value > $max) {
-            return $max;
-        } elseif ($value < $min) {
-            return $min;
-        }
-        return $value;
+/**
+ * Clamps a value between a minimum and maximum range.
+ *
+ * @param  int|float|string  $value  The value to be clamped.
+ * @param  int|float|string  $min  The minimum value of the range.
+ * @param  int|float|string  $max  The maximum value of the range.
+ *
+ * @return int|float|string The clamped value.
+ */
+function clamp(int|float|string $value, int|float|string $min, int|float|string $max): int|float|string
+{
+    if ($value > $max) {
+        return $max;
+    } elseif ($value < $min) {
+        return $min;
     }
+    return $value;
 }
 
-if (!function_exists('clampMinMax')) {
-    /**
-     * Clamps a value between a minimum and maximum range. This function uses the max($min, min($max, $value)) approach.
-     *
-     * @param  int|float|string  $value  The value to be clamped.
-     * @param  int|float|string  $min  The minimum value of the range.
-     * @param  int|float|string  $max  The maximum value of the range.
-     *
-     * @return int|float|string The clamped value.
-     */
-    function clampMinMax(int|float|string $value, int|float|string $min, int|float|string $max): int|float|string
-    {
-        return max($min, min($max, $value));
-    }
+/**
+ * Clamps a value between a minimum and maximum range. This function uses the max($min, min($max, $value)) approach.
+ *
+ * @param  int|float|string  $value  The value to be clamped.
+ * @param  int|float|string  $min  The minimum value of the range.
+ * @param  int|float|string  $max  The maximum value of the range.
+ *
+ * @return int|float|string The clamped value.
+ */
+function clampMinMax(int|float|string $value, int|float|string $min, int|float|string $max): int|float|string
+{
+    return max($min, min($max, $value));
 }
