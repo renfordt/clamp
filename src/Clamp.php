7e0115a5b4ea3ff9ef4020aac9d2b8b68ecf50f6
Jannik Renfordt <jannik.renfordt@ikobb.de>
2024-04-14 20:54:34 +0200

    Revise terminology in clamp function comments
    This commit changes the term 'number' to 'value' and explains the 'min' and 'max' parameters as the range within the documentation for the clamp function. These adjustments make the comments more accurate and clear regarding the function's purpose and parameters.

--- src/Clamp.php
+++ src/Clamp.php
@@ -2,13 +2,13 @@
 
 if (!function_exists('clamp')) {
     /**
-     * Clamps a given number between a minimum and maximum value.
+     * Clamps a value between a minimum and maximum range.
      *
-     * @param  int|float|string  $value  The number to be clamped.
-     * @param  int|float|string  $min  The minimum value to clamp to.
-     * @param  int|float|string  $max  The maximum value to clamp to.
+     * @param  int|float|string  $value  The value to be clamped.
+     * @param  int|float|string  $min  The minimum value of the range.
+     * @param  int|float|string  $max  The maximum value of the range.
      *
-     * @return int|float|string The clamped number.
+     * @return int|float|string The clamped value.
      */
     function clamp(int|float|string $value, int|float|string $min, int|float|string $max): int|float|string
     {
