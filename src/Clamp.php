76356b0544eb030132aa3d9b4b28d4dd45248b74
Jannik Renfordt <jannik.renfordt@ikobb.de>
2024-04-14 21:14:32 +0200

    Add 'clampMinMax' function to Clamp.php
    A new function named 'clampMinMax' has been added to the 'Clamp.php' file. This function provides functionality to clamp a value within a specified minimum and maximum range, using the max($min, min($max, $value)) approach. It handles inputs of types int, float, or string.

--- src/Clamp.php
+++ src/Clamp.php
@@ -20,3 +20,19 @@ if (!function_exists('clamp')) {
         return $value;
     }
 }
+
+if (!function_exists('clampMinMax')) {
+    /**
+     * Clamps a value between a minimum and maximum range. This function uses the max($min, min($max, $value)) approach.
+     *
+     * @param  int|float|string  $value  The value to be clamped.
+     * @param  int|float|string  $min  The minimum value of the range.
+     * @param  int|float|string  $max  The maximum value of the range.
+     *
+     * @return int|float|string The clamped value.
+     */
+    function clampMinMax(int|float|string $value, int|float|string $min, int|float|string $max): int|float|string
+    {
+        return max($min, min($max, $value));
+    }
+}
